688cb4e9cf8e6372859b19477ddf467b409c5153
Mark Triggs <mark@dishevelled.net>
2015-10-12 13:15:31 +1100

    Add missing 'suppressed' column to relationship

--- migrations/005_add_was_linked_column.rb
+++ migrations/005_add_was_linked_column.rb
@@ -6,6 +6,11 @@ Sequel.migration do
     alter_table(:onbase_document) do
       add_column(:was_linked, Integer, :null => false, :default => 0)
     end
+
+    alter_table(:onbase_document_rlshp) do
+      add_column(:suppressed, Integer, :null => false, :default => 0)
+    end
+
   end
 
 end
